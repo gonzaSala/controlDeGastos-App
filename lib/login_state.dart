298c847d61908b24efec0e9435c0234ab964acd5
gonzaSala <gonzasalamendi@gmail.com>
2023-10-24 19:52:06 -0300

    google auth

--- lib/login_state.dart
+++ lib/login_state.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginState with ChangeNotifier {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   bool loggedIn = false;
   bool isLoggedIn() => loggedIn;
-  void login() {
+
+  void login() async {
+    var user = handleSignIn();
     loggedIn = true;
     notifyListeners();
   }
@@ -11,5 +17,34 @@ class LoginState with ChangeNotifier {
   void logout() {
     loggedIn = false;
     notifyListeners();
+  }
+
+  Future<User?> handleSignIn() async {
+    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+
+    if (googleUser == null) {
+      return null;
+    }
+
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    try {
+      final UserCredential authResult =
+          await firebaseAuth.signInWithCredential(credential);
+
+      final User? user = authResult.user;
+
+      print('Inicio de sesión exitoso: ${user?.displayName}');
+      return user;
+    } catch (error) {
+      print('Error al iniciar sesión con Google: $error');
+      return null;
+    }
   }
 }
