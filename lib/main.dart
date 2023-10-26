15d86e4d36721b2b87d7ec0fd7c884214b2ba812
gonzaSala <gonzasalamendi@gmail.com>
2023-10-25 21:43:08 -0300

    un poco de colr, pero igual no esta bien

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Material App',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.orange,
         ),
         routes: {
           '/': (BuildContext context) {
