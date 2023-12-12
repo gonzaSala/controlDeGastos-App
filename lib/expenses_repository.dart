a816c38b22ea525d782cbb6beb38ae3133de1106
gonzaSala <gonzasalamendi@gmail.com>
2023-12-11 21:02:43 -0300

    agregamos mas settings

--- lib/expenses_repository.dart
+++ lib/expenses_repository.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class expensesRepository {
   final String? userId;
@@ -63,5 +67,78 @@ class expensesRepository {
       'details': details,
       'timestamp': DateTime.now(),
     });
+  }
+
+  Future<void> resetApp() async {
+    try {
+      await _deleteCollection('user/$userId/expenses');
+      print('Firestore reset successful');
+    } catch (e) {
+      print('Error resetting Firestore: $e');
+    }
+  }
+
+  Future<void> _deleteCollection(String collectionPath) async {
+    var collectionRef = FirebaseFirestore.instance.collection(collectionPath);
+    var snapshots = await collectionRef.get();
+    for (var doc in snapshots.docs) {
+      await doc.reference.delete();
+    }
+  }
+
+  Future<void> deleteMonthCurrent() async {
+    final now = DateTime.now();
+    final currentMonth = now.month;
+    final currentYear = now.year;
+
+    final expensesSnapshot = await FirebaseFirestore.instance
+        .collection('user')
+        .doc(userId)
+        .collection('expenses')
+        .where('month', isEqualTo: currentMonth)
+        .where('year', isEqualTo: currentYear)
+        .get();
+
+    for (var doc in expensesSnapshot.docs) {
+      await doc.reference.delete();
+    }
+  }
+
+  final FirebaseStorage storage = FirebaseStorage.instance;
+
+  Future<bool> uploadImg(File image) async {
+    final String nameFile = image.path.split('/').last;
+
+    final Reference ref = storage.ref().child('Images').child(nameFile);
+    final UploadTask uploadTask = ref.putFile(image);
+
+    final TaskSnapshot snapshot = await uploadTask.whenComplete(() => true);
+
+    final String url = await snapshot.ref.getDownloadURL();
+
+    if (snapshot.state == TaskState.success) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future getImg() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+    return image;
+  }
+
+  Future<String?> getProfileImageUrl() async {
+    try {
+      final url =
+          await storage.ref().child('Images').child(userId!).getDownloadURL();
+
+      return url;
+    } catch (e) {
+      print('Error getting profile image URL: $e');
+      return null;
+    }
   }
 }
--- lib/screens/settingScreen.dart
+++ lib/screens/settingScreen.dart
@@ -1,42 +1,299 @@
+import 'dart:io';
+
+import 'package:control_gastos/expenses_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:control_gastos/firebase_Api.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  File? _selectedImage;
   @override
   Widget build(BuildContext context) {
-    var firebaseApi = Provider.of<FirebaseApi>(context);
-
+    var expensesRepo = Provider.of<expensesRepository>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Opciones'),
+        foregroundColor: Colors.white,
+        title: Text(
+          'Opciones',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
+        children: [
+          _uploadProfilePicture(context, expensesRepo),
+          _notifications(context),
+          _deleteData(context, expensesRepo),
+        ],
+      ),
+    );
+  }
+
+  Widget _uploadProfilePicture(BuildContext context, expensesRepository repo) {
+    return Container(
+      width: double.infinity,
+      alignment: Alignment.topLeft,
+      color: Color.fromARGB(23, 163, 163, 163),
+      padding: EdgeInsets.all(8),
+      child: Container(
+        width: 120,
+        height: 120,
+        child: Stack(
+          alignment: AlignmentDirectional.centerStart,
           children: [
-            Text(
-              'Notificaciones',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-            Switch(
-              activeColor: Color.fromARGB(255, 216, 255, 203),
-              activeTrackColor: const Color.fromARGB(255, 255, 255, 255),
-              inactiveThumbColor: Colors.grey.withOpacity(0.7),
-              inactiveTrackColor: Colors.grey.withOpacity(0.5),
-              splashRadius: 80.0,
-              value: firebaseApi.isNotificationsEnabled,
-              onChanged: (bool value) {
-                _toggleNotifications(context, value);
-                print('$_toggleNotifications');
-              },
+            _loadProfileImage(context, repo),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: FloatingActionButton(
+                onPressed: () async {
+                  await _pickImage(context, repo);
+                },
+                backgroundColor: Color.fromARGB(135, 162, 162, 162),
+                mini: true,
+                child: Icon(Icons.add),
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _loadProfileImage(BuildContext context, expensesRepository repo) {
+    if (_selectedImage != null) {
+      return CircleAvatar(
+        radius: 50,
+        backgroundImage: FileImage(_selectedImage!),
+      );
+    } else {
+      return CircleAvatar(
+        radius: 50,
+        child: Icon(Icons.person),
+      );
+    }
+  }
+
+  Future<void> _pickImage(BuildContext context, expensesRepository repo) async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      _selectedImage = File(image.path);
+
+      final success = await repo.uploadImg(_selectedImage!);
+
+      if (success) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Imagen cargada con éxito',
+                style: TextStyle(color: Colors.white)),
+            duration: Duration(seconds: 2),
+          ),
+        );
+        setState(() {
+          _loadProfileImage(context, repo);
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error al cargar la imagen',
+                style: TextStyle(color: Colors.white)),
+            duration: Duration(seconds: 2),
+          ),
+        );
+      }
+    }
+  }
+
+  Widget _deleteData(BuildContext context, expensesRepository repo) {
+    return Container(
+      alignment: Alignment.topLeft,
+      color: Color.fromARGB(23, 163, 163, 163),
+      padding: EdgeInsets.all(8),
+      child: TextButton(
+        onPressed: () {
+          _selectDelete(context, repo);
+        },
+        child: Text(
+          'Eliminar gastos',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _selectDelete(BuildContext context, expensesRepository repo) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Color.fromARGB(255, 239, 224, 255),
+          title: Center(child: Text('Seleccione una opción')),
+          content: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(15),
+                  elevation: 10,
+                  backgroundColor: Color.fromARGB(67, 249, 18, 157),
+                  foregroundColor: Color.fromARGB(215, 207, 207, 207),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  textStyle: TextStyle(
+                    fontSize: 10,
+                    letterSpacing: 0.8,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _showConfirmationDialog(context, repo,
+                      deleteCurrentMonth: true);
+                },
+                child: Text('MES ACTUAL'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(15),
+                  elevation: 10,
+                  backgroundColor: Color.fromARGB(67, 249, 18, 157),
+                  foregroundColor: Color.fromARGB(215, 207, 207, 207),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  textStyle: TextStyle(
+                    fontSize: 10,
+                    letterSpacing: 0.8,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _showConfirmationDialog(context, repo,
+                      deleteCurrentMonth: false);
+                },
+                child: Text('TODOS'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _showConfirmationDialog(BuildContext context, expensesRepository repo,
+      {required bool deleteCurrentMonth}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(child: Text('Confirmación')),
+          content: Text(
+            deleteCurrentMonth
+                ? '¿Está seguro de que desea eliminar todos los gastos del mes actual?'
+                : '¿Está seguro de que desea eliminar todos los gastos?',
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(10),
+                elevation: 10,
+                backgroundColor: Color.fromARGB(67, 249, 18, 157),
+                foregroundColor: Color.fromARGB(215, 207, 207, 207),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  letterSpacing: 0.8,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (deleteCurrentMonth) {
+                  await repo.deleteMonthCurrent();
+                  print('se elimino lo del mes actual');
+                } else {
+                  await repo.resetApp();
+                  print('se elimino TODO');
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Confirmar',
+                style: TextStyle(color: Color.fromARGB(255, 255, 100, 100)),
+              ),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(10),
+                elevation: 10,
+                backgroundColor: Color.fromARGB(67, 249, 18, 157),
+                foregroundColor: Color.fromARGB(215, 207, 207, 207),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  letterSpacing: 0.8,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _notifications(BuildContext context) {
+    var firebaseApi = Provider.of<FirebaseApi>(context);
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Notificaciones',
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Switch(
+            activeColor: Color.fromARGB(170, 205, 154, 255),
+            activeTrackColor: Color.fromARGB(205, 181, 178, 178),
+            inactiveThumbColor: Color.fromARGB(170, 56, 4, 107),
+            inactiveTrackColor:
+                const Color.fromARGB(123, 158, 158, 158).withOpacity(0.5),
+            splashRadius: 80.0,
+            value: firebaseApi.isNotificationsEnabled,
+            onChanged: (bool value) {
+              _toggleNotifications(context, value);
+              print('$_toggleNotifications');
+            },
+          ),
+        ],
       ),
     );
   }
